6111d76f5e88a7c2e8944cb3196cfc21e225a042
srw.ing <64050697@kmitl.ac.th>
2024-01-09 19:37:30 +0700

    Lab6.3

---
+++ ServerWOFacade.java
@@ -0,0 +1,8 @@
+public class ServerWOFacade {
+    public static void main(String[] args) {
+        ScheduleServer scheduleServer = new ScheduleServer();
+        ServerFacade serverFacade = new ServerFacade(scheduleServer);
+        serverFacade.startServer();
+        serverFacade.stopServer();
+    }
+}
